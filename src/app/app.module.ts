55e5c22dd28d47ab565d724a1434052a0d3fe5a5
juantorres550 <torresjuan550@gmail.com>
2023-05-08 12:02:35 -0500

    Country App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { CountriesModule } from './countries/countries.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    
+    SharedModule,
+    CountriesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
